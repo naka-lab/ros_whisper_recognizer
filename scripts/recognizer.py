b8a71fb63138cd4c9ece49cdd006ef0cdf8fac25
nakatomo <nakalab@example.com>
2022-10-19 18:28:27 +0900

    グラフ表示機能追加

--- scripts/recognizer.py
+++ scripts/recognizer.py
@@ -8,15 +8,17 @@ import whisper
 import yaml
 import rospy
 from std_msgs.msg import String
+import matplotlib.pyplot as plt
+import matplotlib
 
 # Whisperの設定
-whisper_options = whisper.DecodingOptions(language="japanese")
+whisper_options = whisper.DecodingOptions(language="japanese", fp16=False) # 型でエラーが起きる場合はfp16の値を変える
 model = whisper.load_model("small").to("cuda")
 
 # VADの設定
 VAD_POWER_THRESHOLD = 2000000
 VAD_SILLEN_THRESHOLD = 10  # 15が約1秒
-VAD_MIN_LENGTH_SPEECH = 10 # 15が約1秒
+VAD_MIN_LENGTH_SPEECH = 15 # 15が約1秒
 VAD_MAX_LENGTH_SPEECH = 150 # 15が約1秒
 
 # 録音設定
@@ -33,6 +35,10 @@ for x in range(0, audio.get_device_count()):
     print(info["index"], ":" , info["name"])
 DEVICE_INDEX = int( input("Select device -> ") )
 
+# 表示設定
+matplotlib.use('TkAgg')
+GRAPH_MAX_LEN = 150
+
 
 def record( stream ):
     frames = []
@@ -40,6 +46,11 @@ def record( stream ):
     is_active = False
     activity_list = []
     
+    # グラフ表示用
+    disp_powers = []
+    dsip_acitivities = []
+    
+    data = stream.read(CHUNK)
     while not rospy.is_shutdown():
         data = stream.read(CHUNK)
         data = np.array(np.frombuffer(data, dtype=np.int16), dtype=np.float32 )
@@ -72,7 +83,31 @@ def record( stream ):
         if is_active:
             print(">", end="")
             frames.append( data )
+        else:
+            # 発話途中からの入力にならないように，最新の2つは保持する
+            frames.append(data)
+            if len(frames)>2:
+                frames.pop(0)
+
         sys.stdout.flush()
+
+        # グラフ表示
+        disp_powers.append(power)
+        if len(disp_powers)>GRAPH_MAX_LEN:
+            disp_powers.pop(0)
+
+        dsip_acitivities.append(int(is_active))
+        if len(dsip_acitivities)>GRAPH_MAX_LEN:
+            dsip_acitivities.pop(0)
+
+        plt.clf()
+        plt.plot(range(len(disp_powers)), disp_powers )
+        plt.plot( [0,GRAPH_MAX_LEN], [VAD_POWER_THRESHOLD, VAD_POWER_THRESHOLD] )
+        plt.fill_between( range(len(dsip_acitivities)), np.zeros(len(dsip_acitivities)), np.array(dsip_acitivities)*VAD_POWER_THRESHOLD*1.5, color="r", alpha=0.3 )
+        plt.ylim( 0, VAD_POWER_THRESHOLD*1.5  )
+        plt.draw()
+        plt.pause(0.001)
+
 
     stream.stop_stream()
         
