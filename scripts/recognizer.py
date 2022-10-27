fd9eb2eda865ff24862fe46f1f3b4b92c6b8e5cb
naka-tomo <test@test>
2022-10-27 10:48:57 +0900

    vad変更

--- scripts/recognizer.py
+++ scripts/recognizer.py
@@ -3,6 +3,7 @@
 import pyaudio
 import time
 import sys
+import os
 import numpy as np
 import whisper
 import yaml
@@ -10,16 +11,22 @@ import rospy
 from std_msgs.msg import String
 import matplotlib.pyplot as plt
 import matplotlib
+import torch
 
 # Whisperの設定
-whisper_options = whisper.DecodingOptions(language="japanese", fp16=False) # 型でエラーが起きる場合はfp16の値を変える
-model = whisper.load_model("small").to("cuda")
+DEVICE = "cuda"
+whisper_options = whisper.DecodingOptions(language="japanese", fp16=False)
+model = whisper.load_model("small").to(DEVICE)
 LOGPROB_THRESHOLD = -1.0
 NO_SPEECH_PROB_THRESHOLD = 0.6
 
 # VADの設定
-VAD_POWER_THRESHOLD = 2000000
-VAD_SILLEN_THRESHOLD = 10  # 15が約1秒
+torch.set_grad_enabled(False)
+vad_model = torch.jit.load( os.path.join(os.path.dirname(__file__), "silero_vad.jit"), map_location=DEVICE )
+vad_model.eval()
+
+VAD_PROB_THRESHOLD = 0.5
+VAD_SILLEN_THRESHOLD = 5  # 15が約1秒
 VAD_MIN_LENGTH_SPEECH = 15 # 15が約1秒
 VAD_MAX_LENGTH_SPEECH = 150 # 15が約1秒
 
@@ -42,82 +49,97 @@ matplotlib.use('TkAgg')
 GRAPH_MAX_LEN = 150
 
 
-def record( stream ):
-    frames = []
 
+def record( stream ):
+    chunks = []
     is_active = False
-    activity_list = []
+    num_silent_frames = 0
     
     # グラフ表示用
-    disp_powers = []
-    dsip_acitivities = []
+    powers = []
+    acitivities = []
+    vad_probs = []
     
+    stream.start_stream()
     data = stream.read(CHUNK)
     while not rospy.is_shutdown():
         data = stream.read(CHUNK)
-        data = np.array(np.frombuffer(data, dtype=np.int16), dtype=np.float32 )
-        power = np.sum(data**2)/CHUNK
+        data = np.array(np.frombuffer(data, dtype=np.int16), dtype=np.float32 )/32767
 
-        if power>VAD_POWER_THRESHOLD:
-            is_active = True
-            activity_list.append(1)
-        elif is_active:
-            activity_list.append(0)
+        m = np.max(np.abs(data))
+        if m!=0:
+            data_vad = data/m
         else:
-            activity_list = []
+            data_vad = data
+
+        vad_prob = vad_model(torch.from_numpy(data_vad).to(DEVICE),RATE)[0][0].item()
+
+        if vad_prob>VAD_PROB_THRESHOLD:
+            is_active = True
+            num_silent_frames = 0
+        elif is_active:
+            num_silent_frames += 1
+        else:
+            num_silent_frames = 0
         
-        if is_active and len(activity_list)>VAD_SILLEN_THRESHOLD and np.sum(activity_list[-VAD_SILLEN_THRESHOLD:])==0:
-            if len(activity_list)>VAD_MIN_LENGTH_SPEECH:
+        if is_active and num_silent_frames>VAD_SILLEN_THRESHOLD:
+            if len(chunks)>VAD_MIN_LENGTH_SPEECH:
                 print(" Detected!")
-                return np.hstack( frames )
+                stream.stop_stream()
+                return np.hstack( chunks )
             else:
                 print(" Too short....")
             is_active = False
-            activity_list = []
-            frames = []
+            num_silent_frames = 0
+            chunks = []
 
-        elif is_active and len(activity_list)>VAD_MAX_LENGTH_SPEECH:
+        elif is_active and len(chunks)>VAD_MAX_LENGTH_SPEECH:
             is_active = False
-            activity_list = []
-            frames = []
+            num_silent_frames = 0
+            chunks = []
             print( " Too long..." )
         
         if is_active:
             print(">", end="")
-            frames.append( data )
+            chunks.append( data )
         else:
             # 発話途中からの入力にならないように，最新の2つは保持する
-            frames.append(data)
-            if len(frames)>2:
-                frames.pop(0)
+            chunks.append(data)
+            if len(chunks)>2:
+                chunks.pop(0)
 
         sys.stdout.flush()
 
         # グラフ表示
-        disp_powers.append(power)
-        if len(disp_powers)>GRAPH_MAX_LEN:
-            disp_powers.pop(0)
+        powers.append( np.sum(data**2)/CHUNK )
+        if len(powers)>GRAPH_MAX_LEN:
+            powers.pop(0)
 
-        dsip_acitivities.append(int(is_active))
-        if len(dsip_acitivities)>GRAPH_MAX_LEN:
-            dsip_acitivities.pop(0)
+        acitivities.append(int(is_active))
+        if len(acitivities)>GRAPH_MAX_LEN:
+            acitivities.pop(0)
+
+        vad_probs.append(vad_prob)
+        if len(vad_probs)>GRAPH_MAX_LEN:
+            vad_probs.pop(0)
 
         plt.clf()
-        plt.plot(range(len(disp_powers)), disp_powers )
-        plt.plot( [0,GRAPH_MAX_LEN], [VAD_POWER_THRESHOLD, VAD_POWER_THRESHOLD] )
-        plt.fill_between( range(len(dsip_acitivities)), np.zeros(len(dsip_acitivities)), np.array(dsip_acitivities)*VAD_POWER_THRESHOLD*1.5, color="r", alpha=0.3 )
-        plt.ylim( 0, VAD_POWER_THRESHOLD*1.5  )
+        plt.plot( range(len(powers)), powers, label="power" )
+        plt.plot( range(len(vad_probs)), vad_probs, label="vad_prob" )
+        plt.plot( [0,GRAPH_MAX_LEN], [VAD_PROB_THRESHOLD, VAD_PROB_THRESHOLD], label="vad_prob_threshold" )
+        plt.fill_between( range(len(acitivities)), np.zeros(len(acitivities)), np.array(acitivities), color="r", alpha=0.3 )
+        plt.ylim( 0, 1 )
+        plt.legend()
         plt.draw()
         plt.pause(0.001)
 
-
     stream.stop_stream()
-        
-    return frames
+    return None
+
 
 def recognize(wave):
     start = time.time()
-    wave = whisper.pad_or_trim(wave/32767)
+    wave = whisper.pad_or_trim(wave)
     mel = whisper.log_mel_spectrogram(wave).to(model.device)
     #_, probs = model.detect_language(mel)
     result = whisper.decode(model, mel, whisper_options)
@@ -133,7 +155,6 @@ def recognize(wave):
     else:
         print("  -> Accepted")
         return result 
-
 
 def main():
     stream = audio.open(format=FORMAT,
@@ -156,9 +177,7 @@ def main():
             pub_recres_nbest.publish( yaml.dump([{"text":result.text, "conf":result.avg_logprob}]) )
 
     stream.close()
-
     return 
-
 
 if __name__ == "__main__":
     rospy.init_node('whisper_recognizer' )
